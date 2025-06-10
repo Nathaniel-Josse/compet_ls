6badaaa1f85f43b46b1d079702332cdf17af484f
Sébastien <lopezsebastien.pro@gmail.com>
2025-06-11 00:32:13 +0200

    Most of the functional V1 done | Need verification and correction on front alongside possible optimization

---
+++ front/src/app/register/page.tsx
@@ -0,0 +1,90 @@
+"use client";
+import { useState } from 'react';
+import Link from 'next/link';
+import styles from './register.module.css';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+
+export default function Register() {
+    const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+    const [person, setPerson] = useState(1);
+    const [surface, setSurface] = useState(80);
+    const [chauffage, setChauffage] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+    const router = useRouter();
+
+    // const limit_notif = {"80": 0, "100": 0, "120": 0, "140": 0};
+
+    const handleRegister = async (e: React.FormEvent) => {
+        e.preventDefault();
+        if (password !== confirmPassword) {
+            alert('Les mots de passe ne correspondent pas.');
+            return;
+        }
+        // Here you would typically send a request to your backend to register the user
+        const res = await fetch('http://localhost:5000/api/auth/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ name, email, password }),
+        });
+
+        const data = await res.json();
+
+        if (res.ok) {
+            const statRes = await fetch('http://localhost:5000/api/stats/add', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ user_id: data.userId, person, surface, chauffage}),
+            });
+
+            const statData = await statRes.json();
+            if (!statRes.ok) {
+                console.error('Erreur lors de la création des statistiques :', statData.message);
+            }
+
+            alert('Inscription réussie ! Vous allez être redirigé vers la page de connexion.');
+            setTimeout(() => {
+                    router.push('/login');
+                }, 2000);
+        } else {
+            alert('Erreur d\'inscription : ' + data.message);
+        }
+    };
+
+    return (
+        <div className={styles.container}>
+            <div>
+                <Image src="/images/logo_lumea_rect_transp.png" alt="logo" width={0} height={0} sizes="30vw" style={{ width: '30vw', height: 'auto' }} />
+            </div>
+
+            <form onSubmit={handleRegister} className={styles.form}>
+                <input type="text" value={name} onChange={e => setName(e.target.value)} required placeholder="Nom" />
+                <input type="email" value={email} onChange={e => setEmail(e.target.value)} required placeholder="Email" />
+                <input type="password" value={password} onChange={e => setPassword(e.target.value)} required placeholder="Mot de passe" />
+                <input type="password" value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)} required placeholder="Confirmer le mot de passe" />
+                <input type="number" value={person} onChange={e => setPerson(Number(e.target.value))} required placeholder="1" />
+                <select value={surface} onChange={e => setSurface(Number(e.target.value))} required>
+                    <option value="" disabled>Surface du logement (m²)</option>
+                    <option value={80}>80</option>
+                    <option value={100}>100</option>
+                    <option value={120}>120</option>
+                    <option value={140}>140</option>
+                </select>
+                <select value={chauffage} onChange={e => setChauffage(e.target.value)} required>
+                    <option value="" disabled>Type de Chauffage</option>
+                    <option value="électrique">Électrique</option>
+                    <option value="autre">Autre</option>
+                </select>
+                <button type="submit">S&apos;inscrire</button>
+            </form>
+            Déjà inscrit ? <Link href="/login">Connectez-vous</Link>
+        </div>
+    );
+}
---
+++ front/src/components/consommation/consommation.tsx
@@ -0,0 +1,143 @@
+'use client';
+import Image from 'next/image';
+import styles from './consommation.module.css';
+import { useEffect, useState } from 'react';
+
+type StatsData = {
+    currentConsumption: number;
+    dailyConsumption: number;
+    monthlyConsumption: number;
+    // add other properties if needed
+};
+
+export default function Consommation() {
+    const [data, setData] = useState<StatsData | null>(null);
+
+    useEffect(() => {
+        const fetchStats = async () => {
+            const token = localStorage.getItem('token');
+            const tokenRefresh = localStorage.getItem('tokenRefresh');
+            if (!token) return;
+
+            try {
+                const profileRes = await fetch("http://localhost:5000/api/auth/profile", {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
+
+                if (!profileRes.ok) {
+                    const newRes = await fetch("http://localhost:5000/api/auth/refresh-token", {
+                        headers: {
+                            Authorization: `Bearer ${tokenRefresh }`,
+                        },
+                    });
+                    if (!newRes.ok) {
+                        throw new Error("Failed to fetch user profile");
+                    }
+
+                    const newData = await newRes.json();
+                    localStorage.setItem("token", newData.token);
+                    fetchStats(); // Retry fetching user profile with new token
+                    return;
+                }
+
+                const profileData = await profileRes.json();
+                const signupData = profileData?.user_date;
+
+                if (!signupData) {
+                    console.error("No signup data found in profile");
+                    return;
+                }
+
+                const statsRes = await fetch(`http://localhost:5000/api/stats/user?signupDate=${signupData}`, {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
+
+                if (!statsRes.ok) {
+                    const newRes = await fetch("http://localhost:5000/api/auth/refresh-token", {
+                        headers: {
+                            Authorization: `Bearer ${tokenRefresh }`,
+                        },
+                    });
+                    if (!newRes.ok) {
+                        throw new Error("Failed to fetch user stats");
+                    }
+
+                    const newData = await newRes.json();
+                    localStorage.setItem("token", newData.token);
+                    fetchStats(); // Retry fetching user profile with new token
+                    return;
+                }
+
+                const data = await statsRes.json();
+                setData(data);
+                if (statsRes.ok) {
+                    const total_consumed = [data.currentConsumption, data.dailyConsumption, data.monthlyConsumption, data.lastDayConsumption];
+
+                    const saveRes = await fetch("http://localhost:5000/api/stats/update", {
+                    method: 'PUT',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bearer ${token}`,
+                    },
+                    body: JSON.stringify({ total_consumed }),
+                    });
+
+                    const statData = await saveRes.json();
+                    if (!saveRes.ok) {
+                    console.error('Erreur lors de la mise à jour des statistiques :', statData.message);
+                    const newRes = await fetch("http://localhost:5000/api/auth/refresh-token", {
+                        headers: {
+                            Authorization: `Bearer ${tokenRefresh }`,
+                        },
+                    });
+                    if (!newRes.ok) {
+                        throw new Error("Failed to fetch user stats");
+                    }
+
+                    const newData = await newRes.json();
+                    localStorage.setItem("token", newData.token);
+                    fetchStats(); // Retry fetching user profile with new token
+                    return;    
+                    }
+                }
+                
+                
+            } catch (error) {
+                console.error("Error fetching user profile:", error);
+            }
+        }
+        fetchStats();
+    }
+    , []);
+
+    return (
+        <div>
+            <div    className={styles.containerMajor}>
+                <div className={styles.title}>Consommation actuelle</div>
+                <div className={styles.consoNumber}>{data ? `${data.currentConsumption}W` : 'Chargement...'}</div>
+                <div className={styles.minitext}>
+                    <div><Image src="/images/trending-up.svg" alt="Profil" className={styles.icon} width={16} height={16}/> </div>
+                    <div>+12% par rapport à hier</div>
+                </div>
+            </div>
+            <div className='superContainer'>
+                <div className={styles.containerMinor}>
+                    <div className={styles.type} style={{backgroundColor: "#3b482a", borderColor: "#374151"}}>
+                        <Image src="/images/rotate-ccw.svg" alt="Profil" width={16} height={16} color="#bdff5f"/>
+                    </div>
+                    <div>Aujourd&apos;hui<h1>{data ? `${data.dailyConsumption}kWh` : 'Chargement...'}</h1></div>
+                </div>
+                <div className={styles.containerMinor}>
+                    <div className={styles.type} style={{backgroundColor: "#453a1e", borderColor: "#374151"}}>
+                        <Image src="/images/house.svg" alt="Profil" width={16} height={16} color="#E8E8E8"/>
+                    </div>
+                    <div>Ce mois-ci<h1>{data ? `${data.monthlyConsumption}kWh` : 'Chargement...'}</h1></div>
+                </div>
+            </div>
+        </div>
+    );
+}
