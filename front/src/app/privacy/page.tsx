d7662bc22c399d7d5cda5ca148621a04fbbe8f8f
Nathaniel <nathaniel.josse@gmail.com>
2025-06-25 11:46:37 +0200

    quickfixes: typos

--- front/src/app/privacy/page.tsx
+++ front/src/app/privacy/page.tsx
@@ -7,7 +7,7 @@ export default function PrivacyPage() {
             <h1 className={styles.privacyTitle}>Politique de confidentialité</h1>
             <div className={styles.privacyText}><strong>Introduction</strong></div>
             <div className={styles.privacyText}>
-                Devant le développement des nouveaux outils de communication, il est nécessaire de porter une attention particulière à la protection de la vie privée. C’est pourquoi, nous nous engageons à respecter la confidentialité des renseignements personnels que nous collectons.
+                Devant le développement des nouveaux outils de communication, il est nécessaire de porter une attention particulière à la protection de la vie privée. C&apos;est pourquoi, nous nous engageons à respecter la confidentialité des renseignements personnels que nous collectons.
             </div>
             <section>
                 <h2 className={styles.privacySectionTitle}>Collecte des renseignements personnels</h2>
@@ -22,7 +22,7 @@ export default function PrivacyPage() {
                 <h2 className={styles.privacySectionTitle}>Formulaires&nbsp;et interactivité:</h2>
                 <div className={styles.privacyText}>Vos renseignements personnels sont collectés par le biais de formulaire, à savoir :</div>
                 <ul className={styles.privacyList}>
-                    <li>Formulaire d'inscription au site Web</li>
+                    <li>Formulaire d&apos;inscription au site Web</li>
                 </ul>
                 <div className={styles.privacyText}>
                     Nous utilisons les renseignements ainsi collectés pour les finalités suivantes :
@@ -33,22 +33,22 @@ export default function PrivacyPage() {
                     <li>Contact</li>
                 </ul>
                 <div className={styles.privacyText}>
-                    Vos renseignements sont également collectés par le biais de l’interactivité pouvant s’établir entre vous et notre site Web et ce, de la façon suivante:
+                    Vos renseignements sont également collectés par le biais de l&apos;interactivité pouvant s&apos;établir entre vous et notre site Web et ce, de la façon suivante:
                 </div>
                 <div className={styles.privacyText}>
                     Nous utilisons les renseignements ainsi collectés pour les finalités suivantes :
                 </div>
             </section>
             <section>
-                <h2 className={styles.privacySectionTitle}>Droit d’opposition et de retrait</h2>
+                <h2 className={styles.privacySectionTitle}>Droit d&apos;opposition et de retrait</h2>
                 <div className={styles.privacyText}>
-                    Nous nous engageons à vous offrir un droit d’opposition et de retrait quant à vos renseignements personnels.
+                    Nous nous engageons à vous offrir un droit d&apos;opposition et de retrait quant à vos renseignements personnels.
                 </div>
                 <div className={styles.privacyText}>
-                    Le droit d’opposition s’entend comme étant la possiblité offerte aux internautes de refuser que leurs renseignements personnels soient utilisées à certaines fins mentionnées lors de la collecte.
+                    Le droit d&apos;opposition s&apos;entend comme étant la possiblité offerte aux internautes de refuser que leurs renseignements personnels soient utilisées à certaines fins mentionnées lors de la collecte.
                 </div>
                 <div className={styles.privacyText}>
-                    Le droit de retrait s’entend comme étant la possiblité offerte aux internautes de demander à ce que leurs renseignements personnels ne figurent plus, par exemple, dans une liste de diffusion.
+                    Le droit de retrait s&apos;entend comme étant la possiblité offerte aux internautes de demander à ce que leurs renseignements personnels ne figurent plus, par exemple, dans une liste de diffusion.
                 </div>
                 <div className={styles.privacyText}><strong>Pour pouvoir exercer ces droits, vous pouvez :</strong></div>
                 <ul className={styles.privacyList}>
@@ -64,11 +64,11 @@ export default function PrivacyPage() {
                 </ul>
             </section>
             <section>
-                <h2 className={styles.privacySectionTitle}>Droit d’accès</h2>
+                <h2 className={styles.privacySectionTitle}>Droit d&apos;accès</h2>
                 <div className={styles.privacyText}>
-                    Nous nous engageons à reconnaître un droit d’accès et de rectification aux personnes concernées désireuses de consulter, modifier, voire radier les informations les concernant.
+                    Nous nous engageons à reconnaître un droit d&apos;accès et de rectification aux personnes concernées désireuses de consulter, modifier, voire radier les informations les concernant.
                 </div>
-                <div className={styles.privacyText}><strong>L’exercice de ce droit se fera :</strong></div>
+                <div className={styles.privacyText}><strong>L&apos;exercice de ce droit se fera :</strong></div>
                 <ul className={styles.privacyList}>
                     <li>Code postal : 75000</li>
                     <li>Courriel : <a href="mailto:lumea.dc@gmail.com">lumea.dc@gmail.com</a></li>
@@ -94,7 +94,7 @@ export default function PrivacyPage() {
                     <li>Identifiant / mot de passe</li>
                 </ul>
                 <div className={styles.privacyText}>
-                    Nous nous engageons à maintenir un haut degré de confidentialité en intégrant les dernières innovations technologiques permettant d’assurer la confidentialité de vos transactions. Toutefois, comme aucun mécanisme n’offre une sécurité maximale, une part de risque est toujours présente lorsque l’on utilise Internet pour transmettre des renseignements personnels.
+                    Nous nous engageons à maintenir un haut degré de confidentialité en intégrant les dernières innovations technologiques permettant d&apos;assurer la confidentialité de vos transactions. Toutefois, comme aucun mécanisme n&apos;offre une sécurité maximale, une part de risque est toujours présente lorsque l&apos;on utilise Internet pour transmettre des renseignements personnels.
                 </div>
             </section>
             <section>
@@ -107,7 +107,7 @@ export default function PrivacyPage() {
                 </div>
                 <div className={styles.privacyText}>
                     <strong>Généré avec </strong>
-                    <a href="//www.politiquedeconfidentialite.ca" target="_blank" rel="noopener noreferrer">
+                    <a href="https://www.politiquedeconfidentialite.ca" target="_blank" rel="noopener noreferrer">
                         <strong>un générateur de Politique de confidentialité</strong>
                     </a>
                 </div>
